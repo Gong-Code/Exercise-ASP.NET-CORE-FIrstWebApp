2b7bc96261c40f7d482ff041f5d970b4bf92b980
Gong-KYH <gong.moonphruk@student.kyh.se>
2022-03-24 13:47:01 +0100

    Add project files.

---
+++ FIrstWebApp/Pages/Suppliers.cshtml.cs
@@ -0,0 +1,40 @@
+using FIrstWebApp.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace FIrstWebApp.Pages
+{
+    public class SuppliersModel : PageModel
+    {
+        private readonly NorthwindContext _context;
+
+        public SuppliersModel(NorthwindContext context)
+        {
+            _context = context;
+        }
+        public class SupplierViewModel
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string City { get; set; }
+
+        }
+        public List<SupplierViewModel> Suppliers = new List<SupplierViewModel>();
+        public string Weekday { get; set; }
+        public NorthwindContext Context { get; }
+
+        public void OnGet()
+        {
+            Weekday = DateTime.Now.DayOfWeek.ToString();
+
+            Suppliers = _context.Suppliers.Select(s => new SupplierViewModel
+            {
+                City = s.City,
+                Id = s.SupplierId,
+                Name = s.CompanyName
+            } ).ToList();
+
+        }
+    }
+
+}
